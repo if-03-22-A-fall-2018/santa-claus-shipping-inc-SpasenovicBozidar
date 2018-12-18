7178e5e7b20b81b72984a8f8499a0b04b0c4a38f
Bozidar Spasenovic <spasenovic2002@gmail.com>
2018-12-18 11:40:28 +0100

    Not finished, Segmentation fault

--- list.cpp
+++ list.cpp
@@ -1,4 +1,53 @@
 #include <stdlib.h>
 #include "list.h"
 
+struct _node{
+  void* data;
+  Node* next;
+};
 
+struct _list{
+  Node* _head;
+};
+
+List* list_create(){
+  List* list = (List *)malloc(sizeof(struct _list));
+  return list;
+}
+
+
+void list_delete(List *list){
+
+}
+
+void list_add(List *list, void *data){
+  Node newNode = (Node)malloc(sizeof(Node));
+  if (list._head == NULL) {
+    list->_head = newNode;
+  }
+  Node curr = list._head;
+
+  while (curr.next != NULL) {
+    curr = cur.next;
+  }
+  curr.next = newNode;
+}
+
+void list_insert_after(Node *node, void *data){
+
+}
+
+
+Node* list_get_first(List *list){
+  return list._head;
+}
+
+
+
+Node* list_get_next(Node *node){
+
+}
+
+void* list_get_data(Node *node){
+  return 0;
+}
---
+++ tour_plan.cpp
@@ -0,0 +1,21 @@
+#include "tour_plan.h"
+
+
+TourPlan* plan_createTour(ChildDataMgmt *cdm){
+  return 0;
+}
+
+void plan_print(TourPlan *plan){
+
+}
+void plan_delete(TourPlan *plan){
+
+}
+
+/**
+ * Calculates the number of presents a kid receives based on how well he or she behaved
+ * (hint: look at the test case to understand the calculation)
+ */
+int plan_calc_presents(double goodness){
+  return 0;
+}
